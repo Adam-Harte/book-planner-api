dd619d3055e21b2feeebc0b347b032c2efc34cbd
Adam-Harte <adamharte11@hotmail.co.uk>
2022-12-27 19:10:19 +0000

    Characters model created

--- src/models/types/enums.ts
+++ src/models/types/enums.ts
@@ -3,3 +3,52 @@ export enum Genre {
   SCI_FI = 'sci-fi',
   HORROR = 'horror',
 }
+
+export enum CharacterTitle {
+  MR = 'mr',
+  MASTER = 'master',
+  MRS = 'mrs',
+  MISS = 'miss',
+  COMMANDER = 'commander',
+  LIEUTENANT = 'lieutenant',
+  CAPTAIN = 'captain',
+  GENERAL = 'general',
+  ADMIRAL = 'admiral',
+  KING = 'king',
+  QUEEN = 'queen',
+  PRNCE = 'prince',
+  PRINCESS = 'princess',
+  LORD = 'lord',
+  LADY = 'lady',
+  COUNT = 'count',
+  COUNTESS = 'countess',
+  EMPEROR = 'emperor',
+  EMPRESS = 'empress',
+}
+
+export enum CharacterType {
+  MAIN_PROTAGONIST = 'main_protagonist',
+  MAIN_ANTAGONIST = 'main_antagonist',
+  PROTAGONIST = 'protagonist',
+  ANTAGONIST = 'antagonist',
+  ANTI_HERO = 'anti_hero',
+  SIDE_CHARACTER = 'side_character',
+  MINOR_CHARACTER = 'minor_character',
+}
+
+export enum CharacterGender {
+  MALE = 'm',
+  FEMALE = 'f',
+}
+
+export enum HeightMetric {
+  CENTIMETRES = 'cm',
+  INCHES = 'in',
+  FEET = 'ft',
+}
+
+export enum WeightMetric {
+  POUNDS = 'lbs',
+  KILOS = 'kg',
+  STONE = 'st',
+}
