1e94bdb79995f34d9b863f8e990f326c0c52e1d7
Adam-Harte <adamharte11@hotmail.co.uk>
2022-12-28 10:57:05 +0000

    Religions model created (#21)

---
+++ src/models/religions.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Religions {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  description: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
