dc7ae75efa03fb90412327f3275f1edd260e8af8
Adam-Harte <adamharte11@hotmail.co.uk>
2022-12-29 23:16:14 +0000

    auth routes created

---
+++ src/routes/auth.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import { body } from 'express-validator';
+
+import { deleteAccount, login, logout, signup } from '../controllers/auth';
+import { authorization } from '../middlewares/authorization';
+
+export const authRouter = express.Router();
+
+authRouter.post(
+  '/signup',
+  // username must be at most 35 chars long
+  body('username').isLength({ max: 35 }),
+  // email must be an email
+  body('email').isEmail().normalizeEmail(),
+  // password must be strong and at most 20 chars long
+  body('password').isStrongPassword().isLength({ max: 20 }),
+  signup
+);
+
+authRouter.post(
+  '/login', // email must be an email
+  body('email').isEmail().normalizeEmail(),
+  // password must be strong and at least 8 chars long
+  body('password').isStrongPassword().isLength({ max: 20 }),
+  // password must be at most 20 chars long
+  body('password').isLength({ max: 20 }),
+  login
+);
+
+authRouter.get('/logout', authorization, logout);
+
+authRouter.delete('/delete-account', authorization, deleteAccount);
