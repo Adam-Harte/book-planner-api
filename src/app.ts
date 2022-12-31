c083a7da5a703031d47b9efa21ad2d7aa12dc1ed
Adam-Harte <adamharte11@hotmail.co.uk>
2022-12-31 13:38:49 +0000

    express app setup extracted to own file

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+
+import { authRouter } from './routes/auth';
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+export const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use('/api/auth', authRouter);
