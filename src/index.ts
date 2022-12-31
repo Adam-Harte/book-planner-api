a1cc36aea6fef4c74619697140b81195c332049c
Adam-Harte <adamharte11@hotmail.co.uk>
2022-12-31 13:39:19 +0000

    index refactored to use extracted app import

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
 import 'reflect-metadata';
 
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import * as dotenv from 'dotenv';
-import express from 'express';
-import helmet from 'helmet';
-
+import { app } from './app';
 import { AppDataSource } from './dataSource';
-import { authRouter } from './routes/auth';
-
-dotenv.config();
-
-if (!process.env.PORT) {
-  process.exit(1);
-}
-
-const app = express();
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-
-// routes
-app.use('/api/auth', authRouter);
 
 try {
   await AppDataSource.initialize();
