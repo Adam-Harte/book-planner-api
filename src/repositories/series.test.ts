4b32ae338f65dcee1f51058de6b70de6ec5115a7
Adam-Harte <adamharte11@hotmail.co.uk>
2023-01-02 19:40:17 +0000

    series repository test coverage added

---
+++ src/repositories/series.test.ts
@@ -0,0 +1,176 @@
+import { IBackup } from 'pg-mem';
+import { DataSource } from 'typeorm/data-source';
+
+import { generateMockSeries } from '../mockData/series';
+import { generateMockUser } from '../mockData/users';
+import {
+  destroyTestDataSource,
+  setupTestDataSource,
+  testDb,
+} from '../setupTestDb';
+import { getSeriesRepository } from './series';
+import { getUsersRepository } from './users';
+
+describe('Series repository', () => {
+  let testDataSource: DataSource;
+  let dbBackup: IBackup;
+  let usersRepository: any;
+  let seriesRepository: any;
+
+  beforeAll(async () => {
+    testDataSource = await setupTestDataSource();
+    usersRepository = getUsersRepository(testDataSource);
+    seriesRepository = getSeriesRepository(testDataSource);
+    dbBackup = testDb.backup();
+  });
+
+  beforeEach(() => {
+    dbBackup.restore();
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await destroyTestDataSource(testDataSource);
+  });
+
+  it('returns all series found with matching user id', async () => {
+    const fakeUser = generateMockUser();
+
+    const user = await usersRepository.create(fakeUser);
+    const savedUser = await usersRepository.save(user);
+
+    const fakeSeries1 = generateMockSeries(savedUser);
+    const fakeSeries2 = generateMockSeries(savedUser);
+
+    const series1 = await seriesRepository.create(fakeSeries1);
+    await seriesRepository.save(series1);
+
+    const series2 = await seriesRepository.create(fakeSeries2);
+    await seriesRepository.save(series2);
+
+    const result = await seriesRepository.getAllByUserId(savedUser.id);
+
+    const { user: seriesUser1, ...seriesData1 } = series1;
+    const { user: seriesUser2, ...seriesData2 } = series1;
+
+    expect(result).toEqual(expect.arrayContaining([seriesData2, seriesData1]));
+  });
+
+  it('returns empty array if no series found by matching user id', async () => {
+    const fakeUser1 = generateMockUser();
+    const fakeUser2 = generateMockUser();
+
+    const user1 = await usersRepository.create(fakeUser1);
+    await usersRepository.save(user1);
+
+    const user2 = await usersRepository.create(fakeUser2);
+    await usersRepository.save(user2);
+
+    const fakeSeries1 = generateMockSeries(user1);
+    const fakeSeries2 = generateMockSeries(user1);
+
+    const series1 = await seriesRepository.create(fakeSeries1);
+    await seriesRepository.save(series1);
+
+    const series2 = await seriesRepository.create(fakeSeries2);
+    await seriesRepository.save(series2);
+
+    const result = await seriesRepository.getAllByUserId(user2.id);
+
+    expect(result).toEqual([]);
+  });
+
+  it('returns a series found with a specific user id and series id', async () => {
+    const fakeUser = generateMockUser();
+
+    const user = await usersRepository.create(fakeUser);
+    await usersRepository.save(user);
+
+    const fakeSeries1 = generateMockSeries(user);
+
+    const series = await seriesRepository.create(fakeSeries1);
+    await seriesRepository.save(series);
+
+    const result = await seriesRepository.getByUserIdAndSeriesId(
+      user.id,
+      series.id
+    );
+
+    const { user: seriesUser, ...seriesData } = series;
+
+    expect(result).toEqual({
+      ...seriesData,
+    });
+  });
+
+  it('returns a series found with a specific user id and series id and its relations', async () => {
+    const fakeUser = generateMockUser();
+
+    const user = await usersRepository.create(fakeUser);
+    await usersRepository.save(user);
+
+    const fakeSeries1 = generateMockSeries(user);
+
+    const series = await seriesRepository.create(fakeSeries1);
+    await seriesRepository.save(series);
+
+    const result = await seriesRepository.getByUserIdAndSeriesId(
+      user.id,
+      series.id,
+      true
+    );
+
+    const { user: seriesUser, ...seriesData } = series;
+
+    expect(result).toEqual({
+      ...seriesData,
+      books: [],
+      settings: [],
+      worlds: [],
+      characters: [],
+      plots: [],
+      plotReferences: [],
+      magicSystems: [],
+      weapons: [],
+      technologies: [],
+      transports: [],
+      battles: [],
+      groups: [],
+      creatures: [],
+      races: [],
+      languages: [],
+      songs: [],
+      families: [],
+      governments: [],
+      religions: [],
+      gods: [],
+      artifacts: [],
+      legends: [],
+      histories: [],
+      maps: [],
+    });
+  });
+
+  it('returns null if no series found by a specific user id and series id', async () => {
+    const fakeUser1 = generateMockUser();
+    const fakeUser2 = generateMockUser();
+
+    const user1 = await usersRepository.create(fakeUser1);
+    await usersRepository.save(user1);
+
+    const user2 = await usersRepository.create(fakeUser2);
+    await usersRepository.save(user2);
+
+    const fakeSeries1 = generateMockSeries(user1);
+
+    const series = await seriesRepository.create(fakeSeries1);
+    await seriesRepository.save(series);
+
+    const result = await seriesRepository.getByUserIdAndSeriesId(
+      user2.id,
+      series.id
+    );
+
+    expect(result).toBe(null);
+  });
+});
